97cf0b00fa532d927d09e85dbb517c80f8d30703
Macedo <86130318+MacedoCZY@users.noreply.github.com>
2024-10-15 16:33:41 -0300

    Update dotFile.sh

--- dotFile.sh
+++ dotFile.sh
@@ -509,8 +509,8 @@ wget -O MesloLGSBoldItalic 'https://github.com/romkatv/powerlevel10k-media/raw/m
 
 cd ..
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \$ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
