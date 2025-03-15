57ffb9c7b6c2ac1b73204fc4e89c82f565c5eba3
Macedo <86130318+MacedoCZY@users.noreply.github.com>
2025-03-15 13:37:02 -0300

    background lightdm

--- dotFile.sh
+++ dotFile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #if zsh dont run on terminal, exec zsh at line 1 on .bashrc
 #in polybar, if nm-applet don't show icon, edit "tray-position = right", if this none, on config.ini
+#remember, to chose backgrounds in lightdm, edit the archive in /usr/share/lightdm/"any in".d
 sudo apt update -y && sudo apt upgrade -y && sudo apt install i3-wm polybar neovim ranger brightnessctl git alacritty picom snap wget zsh rofi flameshot build-essential nodejs binutils lightdm -y
 
 sudo snap install brave
